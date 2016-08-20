0e2fe15de506bb14b5918ac844714d524825263e
ChrisKipers <ckipers@gmail.com>
2016-08-19 21:26:49 -0400

    simplifed parallelism

--- DocumentRanker.cpp
+++ DocumentRanker.cpp
@@ -1,6 +1,6 @@
 #include "DocumentRanker.h"
 #include <boost/thread/thread.hpp>
-#include <boost/lockfree/queue.hpp>
+#include <atomic>
 
 DocumentRanker::DocumentRanker(TokenVectorCreator* token_vector_creator, TextProcessor* text_processor) :
         token_vector_creator(token_vector_creator), text_processor(text_processor) {}
@@ -10,18 +10,15 @@ vector<float> DocumentRanker::score_documents(const string &query, const vector<
     SparseVector query_vector = token_vector_creator->create_sparse_vector(query_tokens);
     vector<float> document_scores(documents.size());
 
-    boost::lockfree::queue<int> queue(documents.size() + 1);
-    for (int i = 0; i < documents.size(); i++) {
-        queue.push(i);
-    }
+    std::atomic<long> index(0);
 
     boost::thread_group workers;
     auto worker_lambda = [&]() {
-        int* index = new int(1); //This is ugly, not sure what else to do
-        while (queue.pop(*index)) {
-            document_scores[*index] = query_vector.angle(documents[*index]->getTfIdfVector());
+        long current_index = index++;
+        while ( current_index < document_scores.size()) {
+            document_scores[current_index] = query_vector.angle(documents[current_index]->getTfIdfVector());
+            current_index = index++;
         }
-        delete index;
     };
 
     int number_of_workers = 4;
